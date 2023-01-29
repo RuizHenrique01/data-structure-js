f325393fdb2972f60e9ff647611969ce534e1e72
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2023-01-29 14:17:33 -0400

    feat; adding doubly linked list

--- src/js/data-structures/models/linked-list-models.js
+++ src/js/data-structures/models/linked-list-models.js
@@ -1,6 +1,13 @@
-export default class Node {
-    constructor(element){
+export class Node {
+    constructor(element, next){
         this.element = element;
-        this.next = undefined;
+        this.next = next ? next : undefined;
+    }
+}
+
+export class DoublyNode extends Node{
+    constructor(element, next, prev){
+        super(element, next);
+        this.prev = prev;
     }
 }
