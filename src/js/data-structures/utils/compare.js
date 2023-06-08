03f878171baba6ed973d4b9f70390084170eb8c3
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2023-06-08 10:55:33 -0400

    feat: adding Binary Search Tree

---
+++ src/js/data-structures/utils/compare.js
@@ -0,0 +1,7 @@
+export const Compare = {
+    LESS_THAN: -1,
+    BIGGER_THAN: 1,
+    EQUALS: 0
+  };
+  
+  export const DOES_NOT_EXIST = -1;
---
+++ src/js/data-structures/utils/defaultCompare.js
@@ -0,0 +1,8 @@
+import { Compare } from "./compare";
+
+export function defaultCompare(a, b) {
+    if (a === b) {
+        return Compare.EQUALS;
+    }
+    return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
